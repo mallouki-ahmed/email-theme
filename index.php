6c27d9255d58a3f36adbbd786ecaef9980967946
Ahmed Mallouki <55437219+mallouki-ahmed@users.noreply.github.com>
2022-11-07 23:04:55 +0300

    add théme Email html

---
+++ index.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<?php
+$to = 'ahmedmallouki23@gmail.com';
+$from = 'ahmedmallouki23@gmail.com';
+$fromName = 'Ahmed Mallouki';
+
+$subject = "Send HTML Email";
+
+$htmlContent = ' 
+    
+
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<head>
+<!--[if gte mso 9]>
+<xml>
+  <o:OfficeDocumentSettings>
+    <o:AllowPNG/>
+    <o:PixelsPerInch>96</o:PixelsPerInch>
+  </o:OfficeDocumentSettings>
+</xml>
+<![endif]-->
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="x-apple-disable-message-reformatting">
+  <!--[if !mso]><!--><meta http-equiv="X-UA-Compatible" content="IE=edge"><!--<![endif]-->
+  <title></title>
+  
+    <style type="text/css">
+      @media only screen and (min-width: 620px) {
+  .u-row {
+    width: 600px !important;
+  }
+  .u-row .u-col {
+    vertical-align: top;
+  }
+
+  .u-row .u-col-33p33 {
+    width: 199.98px !important;
+  }
+
+  .u-row .u-col-50 {
+    width: 300px !important;
+  }
+
+  .u-row .u-col-100 {
+    width: 600px !important;
+  }
+
+}
+
+@media (max-width: 620px) {
+  .u-row-container {
+    max-width: 100% !important;
+    padding-left: 0px !important;
+    padding-right: 0px !important;
+  }
+  .u-row .u-col {
+    min-width: 320px !important;
+    max-width: 100% !important;
+    display: block !important;
+  }
+  .u-row {
+    width: calc(100% - 40px) !important;
+  }
+  .u-col {
+    width: 100% !important;
+  }
+  .u-col > div {
+    margin: 0 auto;
+  }
+}
+body {
+  margin: 0;
+  padding: 0;
+}
+
+table,
+tr,
+td {
+  vertical-align: top;
+  border-collapse: collapse;
+}
+
+p {
+  margin: 0;
+}
+
+.ie-container table,
+.mso-container table {
+  table-layout: fixed;
+}
+
+* {
+  line-height: inherit;
+}
+
+a[x-apple-data-detectors='true'] {
+  color: inherit !important;
+  text-decoration: none !important;
+}
+
+table, td { color: #ffffff; } #u_body a { color: #0000ee; text-decoration: underline; } @media (max-width: 480px) { #u_content_image_2 .v-src-width { width: auto !important; } #u_content_image_2 .v-src-max-width { max-width: 70% !important; } #u_content_heading_2 .v-font-size { font-size: 34px !important; } #u_content_heading_2 .v-line-height { line-height: 120% !important; } #u_content_text_2 .v-container-padding-padding { padding: 5px 20px 50px !important; } #u_content_image_3 .v-src-width { width: auto !important; } #u_content_image_3 .v-src-max-width { max-width: 60% !important; } #u_content_heading_7 .v-container-padding-padding { padding: 20px 10px 0px !important; } #u_content_text_3 .v-container-padding-padding { padding: 5px 20px 25px !important; } #u_content_image_4 .v-src-width { width: auto !important; } #u_content_image_4 .v-src-max-width { max-width: 60% !important; } #u_content_text_1 .v-container-padding-padding { padding: 5px 20px 50px !important; } #u_content_heading_5 .v-container-padding-padding { padding: 20px 10px 25px !important; } #u_content_heading_5 .v-font-size { font-size: 34px !important; } #u_content_heading_5 .v-color { color: #ffffff !important; } #u_content_button_3 .v-container-padding-padding { padding: 20px 10px 25px !important; } #u_content_button_3 .v-size-width { width: 65% !important; } #u_content_button_1 .v-container-padding-padding { padding: 20px 10px 25px !important; } #u_content_button_1 .v-size-width { width: 65% !important; } #u_content_button_2 .v-container-padding-padding { padding: 20px 10px 50px !important; } #u_content_button_2 .v-size-width { width: 65% !important; } #u_content_text_7 .v-container-padding-padding { padding: 10px 20px 30px !important; } #u_content_text_7 .v-line-height { line-height: 160% !important; } }
+    </style>
+  
+  
+
+<!--[if !mso]><!--><link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet" type="text/css"><link href="https://fonts.googleapis.com/css2?family=Caveat+Brush&display=swap" rel="stylesheet" type="text/css"><!--<![endif]-->
+
+</head>
+
+<body class="clean-body u_body" style="margin: 0;padding: 0;-webkit-text-size-adjust: 100%;background-color: #ecf0f1;color: #ffffff">
+  <!--[if IE]><div class="ie-container"><![endif]-->
+  <!--[if mso]><div class="mso-container"><![endif]-->
+  <table id="u_body" style="border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;min-width: 320px;Margin: 0 auto;background-color: #ecf0f1;width:100%" cellpadding="0" cellspacing="0">
+  <tbody>
+  <tr style="vertical-align: top">
+    <td style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
+    <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td align="center" style="background-color: #ecf0f1;"><![endif]-->
+    
+
+<div class="u-row-container" style="padding: 0px;background-color: transparent">
+  <div class="u-row" style="Margin: 0 auto;min-width: 320px;max-width: 600px;overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;background-color: transparent;">
+    <div style="border-collapse: collapse;display: table;width: 100%;height: 100%;background-color: transparent;">
+      <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding: 0px;background-color: transparent;" align="center"><table cellpadding="0" cellspacing="0" border="0" style="width:600px;"><tr style="background-color: transparent;"><![endif]-->
+      
+<!--[if (mso)|(IE)]><td align="center" width="600" style="background-color: #0f0b0a;width: 600px;padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;" valign="top"><![endif]-->
+<div class="u-col u-col-100" style="max-width: 320px;min-width: 600px;display: table-cell;vertical-align: top;">
+  <div style="background-color: #0f0b0a;height: 100%;width: 100% !important;">
+  <!--[if (!mso)&(!IE)]><!--><div style="height: 100%; padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;"><!--<![endif]-->
+  
+<table id="u_content_image_2" style="font-family:'Montserrat',sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+  <tbody>   
+<table width="100%" cellpadding="0" cellspacing="0" border="0">
+  <tr>
+    <td style="padding-right: 0px;padding-left: 0px;" align="center">
+      
+      <img style="width: 100%;"  align="center" border="0" src="https://mailync.com/uploads/1667837548.png" alt="image" title="image" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: inline-block !important;border: none;height: auto;float: none;width: 55%;max-width: 319px;" width="319" class="v-src-width v-src-max-width"/>
+      
+    </td>
+  </tr>
+</table>
+<table width="100%" cellpadding="0" cellspacing="0" border="0">
+  <tr>
+    <td style="padding-right: 0px;padding-left: 0px;" align="center">
+      
+      <img style="width: 100%;"  align="center" border="0" src="https://mailync.com/uploads/1667832298.jpg" alt="header1" title="image" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: inline-block !important;border: none;height: auto;float: none;width: 55%;max-width: 319px;" width="319" class="v-src-width v-src-max-width"/>
+      
+    </td>
+  </tr>
+</table>
+<table width="100%" cellpadding="0" cellspacing="0" border="0">
+  <tr>
+    <td style="padding-right: 0px;padding-left: 0px;" align="center">
+      
+      <img style="width: 100%;"  align="center" border="0" src="https://mailync.com/uploads/1667839182.png" alt="header1" title="image" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: inline-block !important;border: none;height: auto;float: none;width: 55%;max-width: 319px;" width="319" class="v-src-width v-src-max-width"/>
+      
+    </td>
+  </tr>
+</table>
+<table width="100%" cellpadding="0" cellspacing="0" border="0">
+  <tr>
+    <td style="padding-right: 0px;padding-left: 0px;" align="center">
+      
+      <img style="width: 100%;"  align="center" border="0" src="https://mailync.com/uploads/1667832329.jpg" alt="header1" title="image" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: inline-block !important;border: none;height: auto;float: none;width: 55%;max-width: 319px;" width="319" class="v-src-width v-src-max-width"/>
+      
+    </td>
+  </tr>
+</table>
+<table width="100%" cellpadding="0" cellspacing="0" border="0">
+  <tr>
+    <td style="padding-right: 0px;padding-left: 0px;" align="center">
+      
+      <img style="width: 100%;"  align="center" border="0" src="https://mailync.com/uploads/1667838137.png" alt="header1" title="image" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: inline-block !important;border: none;height: auto;float: none;width: 55%;max-width: 319px;" width="319" class="v-src-width v-src-max-width"/>
+      
+    </td>
+  </tr>
+</table>
+<table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%">
+  <tr >
+    <td align="center" class="social" style="background-image: url('https://mailync.com/uploads/1667837597.png')">
+
+      <a href="https://www.facebook.com/profile.php?id=100085551123342"> <img  src="http://mailync.com/uploads/1667832098.png"
+          alt="facebook" style="width:42px;height:42px;"></a>
+      <a href="https://www.instagram.com/cznburakqat/"><img src="https://mailync.com/uploads/1667832198.png" alt="HTML tutorial"
+          style="width:42px;height:42px;"></a>
+      <a href="https://www.snapchat.com/add/cznburakqatar?share_id=2UmHDk6PJyo&locale=en-GB"><img
+          src="https://mailync.com/uploads/1667832226.png" alt="HTML tutorial"
+          style="width:42px;height:42px;"></a>
+      <a href="https://www.tiktok.com/@cznburakqat?_t=8WxYrxk0Qin&_r=1"><img
+          src="https://mailync.com/uploads/1667832269.png" alt="HTML tutorial"
+          style="width:42px;height:42px;"></a>
+
+
+
+
+
+
+    </td>
+  </tr>
+</table>
+<table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%">
+  <tr class="bg_white">
+    <td class="logo" >
+      <img style="width: 100%;" src="https://mailync.com/uploads/1667837669.png" alt="Footer">
+
+
+    </td>
+  </tr>
+</table>
+  </tbody>
+</table>
+
+
+    </div>
+  </div>
+</div>
+
+
+    <!--[if (mso)|(IE)]></td></tr></table><![endif]-->
+    </td>
+  </tr>
+  </tbody>
+  </table>
+  <!--[if mso]></div><![endif]-->
+  <!--[if IE]></div><![endif]-->
+</body>
+
+</html>
+
+
+// Set content-type header for sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// Additional headers
+$headers .= 'From: '.$fromName.'<'.$from.'>' . "\r\n";
+
+// Send email
+if(mail($to, $subject, $htmlContent, $headers)){
+    echo 'Email has sent successfully.';
+}else{
+    echo 'Email sending failed.';
+}
+?>
